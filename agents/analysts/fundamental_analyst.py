ab4d031c8741185d8b29b0aa7b85daf43cd59b77
Aadhi Senthil Kumar <aadhi.senthilkumar06@gmail.com>
2026-05-22 11:08:50 +1000

    implemented basic fundamentals analyst agent, need to refine prompt more to extract the most info and incorporate market_data in a way

--- agents/analysts/fundamental_analyst.py
+++ agents/analysts/fundamental_analyst.py
@@ -1,4 +1,17 @@
 from graph.state import ResearchState
+from openrouter import OpenRouter 
+import os 
+
 
 def fundamental_analyst_node(state: ResearchState):
-    return {"fundamental_report": "placeholder"}
+    with OpenRouter(api_key=os.getenv("OPENROUTER_API_KEY")) as client: 
+        # makes the api call and produces a response object
+        response = client.chat.send(
+            model="openrouter/free", 
+            messages=[
+                {"role": "system", 
+                 "content":"You are a Fundamentals analyst for Hedge Fund"}
+            ]
+        )
+        
+    return {"fundamental_report": response.choices[0].message.content}
