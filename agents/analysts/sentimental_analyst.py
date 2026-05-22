398831e6ddb79e1173164e7313797c06da684e8c
Aadhi Senthil Kumar <aadhi.senthilkumar06@gmail.com>
2026-05-22 11:12:28 +1000

    implemented sentimental analyst foundation code, need to link yfinance market data and refine prompt

--- agents/analysts/sentimental_analyst.py
+++ agents/analysts/sentimental_analyst.py
@@ -1,4 +1,17 @@
 from graph.state import ResearchState
+from openrouter import OpenRouter 
+import os 
 
 def sentimental_analyst_node(state: ResearchState):
+    with OpenRouter(api_key=os.getenv("OPENROUTER_API_KEY")) as client: 
+        # makes the api call and produces a response object
+        response = client.chat.send(
+            model="openrouter/free", 
+            messages=[
+                {"role": "system", 
+                 "content":"You are a Sentimental analyst for Hedge Fund"}
+            ]
+        )
+        
     return {"sentimental_analyst": "placeholder"}
+
