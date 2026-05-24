8062a4c96bb4fd6e313a87e05dd5c9c50ee84128
Aadhi Senthil Kumar <aadhi.senthilkumar06@gmail.com>
2026-05-24 12:21:57 +1000

    implemented request logic for sentimental analyst, need to get sentiment data organised

--- agents/analysts/sentimental_analyst.py
+++ agents/analysts/sentimental_analyst.py
@@ -1,18 +1,29 @@
 from graph.state import ResearchState
-from openrouter import OpenRouter 
+import requests
 import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def sentimental_analyst_node(state: ResearchState):
-    with OpenRouter(api_key=os.getenv("OPENROUTER_API_KEY")) as client: 
-        # makes the api call and produces a response object
-        response = client.chat.send(
-            model="openrouter/free", 
-            messages=[
-                {"role": "system", 
-                 "content":"You are a Sentimental analyst for Hedge Fund called "
-                 "Senzo Trading"}
+    ticker = state["ticker"]
+    date = state["ticker"]
+    sentimental = 0 
+
+    response = requests.post("https://openrouter.ai/api/v1/chat/completions",
+        headers={
+            "Authorization": f"Bearer {os.getev('OPENROUTER_API_KEY')}", 
+            "Content-Type":"application/json"
+        } ,
+        json={
+            "model": "openrouter/free", 
+            "messages": [
+                {"role": "system", "content": "system_prompt"}, 
+                {"role": "user", "content": "user_prompt"}
             ]
-        )
-        
-    return {"sentimental_analyst": response.choices[0].messages.content}
+        } )
+
+
+    result = response.json()["choices"][0]["messages"]["content"]
+    return {"sentimental_report": result}
 
