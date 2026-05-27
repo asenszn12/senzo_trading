6bde190c7956ae58b1f56a2c98f2d7c387b14917
Aadhi Senthil Kumar <aadhi.senthilkumar06@gmail.com>
2026-05-27 12:13:23 +1000

    testing works for fundamentals, need to change path though

--- agents/analysts/fundamental_analyst.py
+++ agents/analysts/fundamental_analyst.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/Users/aadhi/senzo_trading')
 from graph.state import ResearchState
 from data.market_data import get_fundamentals
 from datetime import date as currTime
@@ -22,7 +24,6 @@ def fundamental_analyst_node(state: ResearchState):
         json={
             "model": "openrouter/free",
             "messages": [
-
                 {"role": "system", "content": "You are a fundamental analyst at Senzo Trading, "
                 "an AI research firm. You will receive raw financial data for a company including "
                 "income statements, balance sheets, and cashflow statements. Interpret this data "
@@ -30,7 +31,7 @@ def fundamental_analyst_node(state: ResearchState):
                 "and any red flags. End with a markdown table of key metrics. Your report will "
                 "be read by a bull and bear researcher who will debate your findings."},
 
-                {"role": "user", "content": f"Analyse the following fundamental data"
+                {"role": "user", "content": f"Analyse the following fundamental data "
                 f"for {ticker} as of {date} until {currTime.today()}. "
                 f"Here is the fundamentals financial data: {fundamentals}"}
             ]
@@ -39,4 +40,10 @@ def fundamental_analyst_node(state: ResearchState):
     result = response.json()["choices"][0]["message"]["content"]
     return {"fundamentals_report": result}
 
-    
+if __name__ == "__main__":
+    test_state = {
+        "ticker": "AAPL",
+        "date": "2026-01-01"
+    }
+    result = fundamental_analyst_node(test_state)
+    print(result)
